49bc4034296ad7a6182f8d150861dd6da3121cb1
gaurav7058 <146345401+gaurav7058@users.noreply.github.com>
2024-06-29 15:17:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+<script>
+    $(document).ready(function(){
+        $('.card-img-top').click(function(){
+            var src = $(this).attr('src');
+            $('#lightboxImage').attr('src', src);
+        });
+    });
+</script>
